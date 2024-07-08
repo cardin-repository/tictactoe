7670de789451e38fd193d436e0a0655001cbd2fb
Cardin <cardinhschool@gmail.com>
2024-07-08 16:02:47 -0400

    initialize basic vars and setup board

---
+++ tictactoe.py
@@ -0,0 +1,23 @@
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+currentPlayer = "X"
+winner = None
+gameRunning = True
+
+
+
+#print game board
+def gameBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2] + " | ")
+    print(board[3] + " | " + board[4] + " | " + board[5] + " | ")
+    print(board[6] + " | " + board[7] + " | " + board[8] + " | ")
+
+gameBoard(board)
+#get player input
+
+#check for win or tie
+
+#switch the player
+
+#check for win or tie again
